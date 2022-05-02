fe3b288ca09185de344a06cc7e608365d1244962
Martin Conte Mac Donell <reflejo@gmail.com>
2022-05-02 10:48:05 -0700

    Remove example dependency

--- jni.cc
+++ jni.cc
@@ -1,8 +1,21 @@
 #include <jni.h>
-
-#include "jni_dep.h"
+#include <stdlib.h>
+#include <string.h>
 
 const char* hello = "Hello JNI";
+
+jstring NewStringLatin1(JNIEnv *env, const char *str) {
+  int len = strlen(str);
+  jchar *str1;
+  str1 = reinterpret_cast<jchar *>(malloc(len * sizeof(jchar)));
+
+  for (int i = 0; i < len; i++) {
+    str1[i] = (unsigned char)str[i];
+  }
+  jstring result = env->NewString(str1, len);
+  free(str1);
+  return result;
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_io_fzed_helloworldjni_Jni_hello(JNIEnv *env, jclass clazz) {
